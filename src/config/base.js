cb9cdcb3d46bca5b34b9aa102875b25fddde8e15
Linsy Joyner <Linsy.Joyner@gmail.com>
2016-09-20 15:22:51 -0400

    connected users rooms to firebase

---
+++ src/config/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+ apiKey: "AIzaSyDnoxYjmFPcZSQWKRNlebHr9n0pkSGOyUw",
+ authDomain: "final-project-34471.firebaseapp.com",
+ databaseURL: "https://final-project-34471.firebaseio.com",
+ storageBucket: "final-project-34471.appspot.com",
+ messagingSenderId: "47028844129"
+})
+export default base
