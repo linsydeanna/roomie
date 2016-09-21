966b67b227093acd725bfb4c70e3a5863c102163
Kevin Chambers <kbchambers2@gmail.com>
2016-09-21 15:07:59 -0400

    add files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './App';
-import './index.css';
-import routes from './config/Routes'
-=======
 import routes from './config/Routes'
 import './styles/index.css';
->>>>>>> master
+
 
 ReactDOM.render(
   routes,
