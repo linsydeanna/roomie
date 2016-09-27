a4b203731317cf744e4ce5287641601bb42d34de
Kevin Chambers <kbchambers2@gmail.com>
2016-09-27 13:42:01 -0400

    Login, App fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,75 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import base from './config/base'
 
 class App extends Component {
- render() {
-   return (
-     <div className="App">
-       {this.props.children}
-     </div>
-   );
- }
+  getChildContext() {
+    return {applicationUser: this.state.user}
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      household: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.authHandler = this.authHandler.bind(this);
+  }
+
+  addHouseholdName(event) {
+    this.setState({
+      household: event.target.value
+    })
+  }
+
+  authHandler(error, data) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log(data)
+      this.setState({
+        user: data.user
+      })
+   // const userAvatar = this.state.user.user.photoURL
+     // this.addPhoto()
+      sessionStorage.setItem('currentUser', JSON.stringify(data.user))
+       this.context.router.push('/registration')
+    }
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    base.authWithOAuthPopup('facebook', this.authHandler)
+    .then(() => {
+      base.post(`${this.state.household}/roommates/${this.state.user.uid}`, {
+        data: {
+         userName: this.state.user.displayName,
+         url: this.state.user.photoURL
+        },
+        then(err){
+          if(!err){
+          }
+        }
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.props.children && React.cloneElement(this.props.children, {onLogin: this.handleClick.bind(this), addHousehold: this.addHouseholdName.bind(this), applicationUser: this.state.user})}
+      </div>
+    );
+  }
+}
+
+App.contextTypes = {
+ router: React.PropTypes.object.isRequired
+}
+
+App.childContextTypes = {
+ applicationUser: React.PropTypes.object.isRequired
 }
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,88 +1,21 @@
 import React, {Component} from 'react';
 import './styles/Login.css';
-import base from './config/base'
 import './styles/App.css';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      household: '',
-      user: ''
-    }
-    this.handleClick = this.handleClick.bind(this);
-    this.authHandler = this.authHandler.bind(this);
-    this.addHouseholdName = this.addHouseholdName.bind(this);
-  }
-
-
-// componentDidMount() {
-//   this.rebaseRef = base.syncState(`final-project-34471`, {
-//     context: this,
-//     state: 'household',
-//     asArray: true
-//   })
-// }
-// componentWillUnmount() {
-//   base.removeBinding(this.rebaseRef)
-// }
-//
-addHouseholdName(event) {
-  console.log("add household name working")
-  this.setState({household: event.target.value})
-  console.log(event.target.value)
-
-}
-
-
-
-  authHandler(error, user) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(user)
-      this.setState({user: user})
-      console.log("is setState working", this.state)
-      // const userAvatar = this.state.user.user.photoURL
-      // this.addPhoto()
-
-      console.log(user.user.photoURL)
-      this.context.router.push('/registration')
-
-    }
-  }
-
-  handleClick(event) {
-    event.preventDefault();
-    console.log("working")
-    base.authWithOAuthPopup('facebook', this.authHandler).then(() => {
-      base.post(`${this.state.household}/roommates/${this.state.user.user.uid}`, {
-        data: {
-          userName: this.state.user.user.displayName,
-          url: this.state.user.user.photoURL
-        },
-        then(err) {
-          if (!err) {
-            console.log("beer")
-          }
-        }
-      });
-    })
-    // rebase method that authenticates a user using an
-    // OAuth popup. Also, takes in an Auth provider and handler
-  }
-
-  render() {
+ render() {
     return (
-      <div className="loginBox">
-
-          <p>Choose Household Name</p>
-        <input onChange={this.addHouseholdName} type="text" className="answerBox" placeholder="Enter Household Name" ref="houseInput"/>
-
       <div className="Login">
-        <button className="loginBtn" onClick={this.handleClick}>Sign in with Facebook</button>
+        <div>
+        <p>Choose Household Name</p>
+        <input onChange={this.props.addHousehold} type="text" className="answerBox" placeholder="Enter Household Name" ref="houseInput"/>
+        </div>
+        <div>
+        <button className="loginBtn" onClick={this.props.onLogin}>Sign in with Facebook</button>
+        </div>
       </div>
-    </div>
-    ); } } Login.contextTypes = {router : React.PropTypes.object.isRequired
+    );
+  }
 }
-export default Login
+
+export default Login;
